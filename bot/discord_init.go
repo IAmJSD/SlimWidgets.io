c6e405cf0fae38a6dfaf776759d32da6c926015e
Jake Gealer <jake@gealer.email>
2019-10-20 16:55:09 +0100

    Maybe this will work?

--- bot/discord_init.go
+++ bot/discord_init.go
@@ -6,6 +6,6 @@ func DiscordInit(session *discordgo.Session) {
 	session.AddHandler(OnReady)
 	session.AddHandler(OnMessage)
 	session.AddHandler(OnReactionAdd)
-	session.AddHandler(OnGuildCreate)
+	session.AddHandler(OnGuildJoin)
 	session.AddHandler(OnGuildDelete)
 }
--- bot/events.go
+++ bot/events.go
@@ -18,7 +18,7 @@ func OnReady(client *discordgo.Session, _ *discordgo.Ready) {
 	SetPresence(client)
 }
 
-func OnGuildCreate(client *discordgo.Session, Guild *discordgo.GuildCreate) {
+func OnGuildJoin(client *discordgo.Session, Guild *discordgo.UserGuild) {
 	_, err := r.Table("guilds").Insert(&GuildDB{
 		Id:          Guild.ID,
 		Description: nil,
